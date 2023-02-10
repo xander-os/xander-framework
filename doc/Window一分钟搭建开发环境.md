23652a92415550d1534a96b2e5af03deffdb0e26
zhrunxin33 <zhrunxin33@gmail.com>
2023-02-10 22:25:48 +0800

    2.0版本 功能增加 1、Admin模块基本架构更新 2、引入Mybatis Generator生成器 3、项目环境Win一键搭建及本地运行

---
+++ doc/Window一分钟搭建开发环境.md
@@ -0,0 +1,2 @@
+命令行切换到doc/docker目录下
+docker-compose -f docker-compose-env-win.yml up -d
---
+++ xander-admin/src/main/java/com/macro/xander/service/UmsResourceService.java
@@ -0,0 +1,10 @@
+package com.macro.xander.service;
+
+/**
+ * @author ：ZhRunXin
+ * @date ：Created in 2023/2/10 15:38
+ * @email ：zhrunxin33@gmail.com
+ * @description：后台资源管理Service
+ */
+public interface UmsResourceService {
+}
